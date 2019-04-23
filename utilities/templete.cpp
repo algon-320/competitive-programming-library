1931d6f25c4078a8900d974d8fbd724623f469ed
algon-320 <algon.0320@gmail.com>
2019-04-23 17:46:37 +0900

    マクロを変更

--- utilities/templete.cpp
+++ utilities/templete.cpp
@@ -2,32 +2,29 @@
 using namespace std;
 #define int long long
 #define stoi stoll
-using ll=long long;
-using vi=vector<int>;
 using pii=pair<int,int>;
-#define ALL(c) begin(c),end(c)
-#define RALL(c) rbegin(c),rend(c)
-#define ITR(i,b,e) for(auto i=(b);i!=(e);++i)
-#define FORE(x,c) for(auto &x:c)
-#define REPF(i,a,n) for(int i=a,i##len=(int)(n);i<i##len;++i)
-#define REP(i,n) REPF(i,0,n)
-#define REPR(i,n) for(int i=(int)(n);i>=0;--i)
-#define SZ(c) ((int)c.size())
-#define CONTAIN(c,x) (c.find(x)!=end(c))
-#define INSEG(l,x,r) ((l)<=(x)&&(x)<(r))
+#define all(c) begin(c),end(c)
+#define rall(c) rbegin(c),rend(c)
+#define fore(x,c) for(auto &&x:c)
+#define rep(i,a,n) for(int i=a,i##len=(int)(n);i<i##len;++i)
+#define rrep(i, a, n) for(int i=(int)(n-1);i>=a;--i)
+#define sz(c) ((int)c.size())
+#define contains(c,x) (c.find(x)!=end(c))
+#define inseg(l,x,r) ((l)<=(x)&&(x)<(r))
 #define dump(...)
 #define pb push_back
 #define _ 0
 const signed INF_=1001001001; const long long INF=1001001001001001001LL;
 const int DX[9]={0,1,0,-1,1,1,-1,-1,0},DY[9]={-1,0,1,0,-1,1,1,-1,0};
 template<class T> ostream& operator<<(ostream &os,const vector<T> &v) {
-    ITR(i,begin(v),end(v))os<<*i<<(i==end(v)-1?"":" ");return os;}
+    for (auto i = begin(v); i != end(v); i++) os<<*i<<(i==end(v)-1?"":" ");return os;}
 template<class T> istream& operator>>(istream &is,vector<T> &v) {
-    ITR(i,begin(v),end(v)) is>>*i;return is;}
+    for (auto i = begin(v); i != end(v); i++) is>>*i;return is;}
 template<class T,class U> istream& operator>>(istream &is, pair<T,U> &p) {
     is>>p.first>>p.second;return is;}
 template<class T, class U> bool chmax(T &a,const U &b){return a<b?a=b,1:0;}
 template<class T, class U> bool chmin(T &a,const U &b){return a>b?a=b,1:0;}
+template <class T> void psum(T& c) {partial_sum(begin(c), end(c), begin(c));}
 template<class T> using heap=priority_queue<T,vector<T>,greater<T>>;
 struct before_main_function {
     before_main_function() {
@@ -36,13 +33,13 @@ struct before_main_function {
             cerr << "\x1b[7m" << "'int' is 'long long'" << "\x1b[m" << endl;
         #endif
         //SKIPEND
-        cin.tie(0); ios::sync_with_stdio(false);
+        cin.tie(0); ios::sync_with_stdio(0);
         cout << setprecision(15) << fixed;
-        #define endl "\n"
+        // #define endl "\n"
     }
 } before_main_function;
 //------------------8<------------------------------------8<--------------------
 //SKIPBEGIN
-#include "tools/debug_local.hpp"
+#include "./tools/debug_local.hpp"
 //SKIPEND
 #define KIDE_PROBLEM_A
